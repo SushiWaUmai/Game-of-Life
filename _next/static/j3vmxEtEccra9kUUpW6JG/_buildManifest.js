878e8398054044756c72d89bab425d4ada058db6
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2022-04-22 08:03:31 +0000

    Deploying to gh-pages from @ SushiWaUmai/Game-of-Life@c5d5f62031757bdff8274d8589cb352e4faf2e8c 🚀

---
+++ _next/static/j3vmxEtEccra9kUUpW6JG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/41e37d3b-40a1e9b232c6f3b9.js","static/chunks/b5016d16-a74d0a8940b15723.js","static/chunks/pages/index-cecaae71e30f3d00.js"],"/_error":["static/chunks/pages/_error-68c8fb2ff52d9081.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
